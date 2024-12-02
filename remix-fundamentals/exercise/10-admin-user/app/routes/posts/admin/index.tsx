554046304299379e432cf8c37622f88decc77684
wolfiee42 <saifalislam2022@gmail.com>
2024-12-02 10:59:53 +0600

    learning how to make routes private.

--- remix-fundamentals/exercise/10-admin-user/app/routes/posts/admin/index.tsx
+++ remix-fundamentals/exercise/10-admin-user/app/routes/posts/admin/index.tsx
@@ -1,8 +1,12 @@
+import type { LoaderArgs } from "@remix-run/node";
+import { json } from "@remix-run/node";
 import { Link } from "@remix-run/react";
+import { requireAdminUser } from "~/session.server";
 
-// 🐨 add a loader here so we can get the request
-// 🐨 call requireAdminUser from session.server with the request
-// 💰 return json({}) (you must return a response)
+export async function loader({ request }: LoaderArgs) {
+  await requireAdminUser(request);
+  return json({});
+}
 
 export default function AdminIndex() {
   return (
