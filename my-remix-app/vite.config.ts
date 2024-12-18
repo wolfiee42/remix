9384530143eaa1fae001df622c9f7da57eae745f
wolfiee42 <saifalislam2022@gmail.com>
2024-12-18 11:16:59 +0600

    feat: understanding how folder structure works in remix.

--- my-remix-app/vite.config.ts
+++ my-remix-app/vite.config.ts
@@ -1,7 +1,8 @@
 import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-
+import { flatRoutes } from "remix-flat-routes";
+import { json } from "react-router";
 declare module "@remix-run/node" {
   interface Future {
     v3_singleFetch: true;
@@ -11,14 +12,48 @@ declare module "@remix-run/node" {
 export default defineConfig({
   plugins: [
     remix({
+      // #1 routing system.
+      // ignoredRouteFiles: ['**/*'],
+      // 	routes: async (defineRoutes) => {
+      // 		return flatRoutes('routes', defineRoutes, {
+      // 			ignoredRouteFiles: [
+      // 				'.*',
+      // 				'**/*.css',
+      // 				'**/*.test.{js,jsx,ts,tsx}',
+      // 				'**/__*.*',
+      // 			],
+      // 		})
+      // 	},
+
+      // #2 routing system
+      ignoredRouteFiles: ['**/*'],
+      routes: manaRoutes,
       future: {
         v3_fetcherPersist: true,
         v3_relativeSplatPath: true,
         v3_throwAbortReason: true,
         v3_singleFetch: true,
         v3_lazyRouteDiscovery: true,
+
       },
     }),
     tsconfigPaths(),
   ],
 });
+
+
+async function manaRoutes(defineRoutes) {
+
+  let routes = flatRoutes("routes", defineRoutes)
+
+  routes = {
+    ...routes,
+    "routes/carrot_+/_index": {
+      id: "routes/carrot_+/_index",
+      parentId: "root",
+      file: "routes/carrot_+/_index.tsx",
+    },
+  }
+
+  return routes
+}
